2eb4c20346b85424bc29fe4d8646d64b0f956631
squid233 <513508220@qq.com>
2020-05-31 10:45:40 +0800

    10 users add

--- src/io/github/squid233/mcbbsuserinfo/user/PrintUser.java
+++ src/io/github/squid233/mcbbsuserinfo/user/PrintUser.java
@@ -16,14 +16,43 @@ public class PrintUser {
         System.out.println("请输入用户名：");
 
         String user = sc.nextLine();
+
         switch (user) {
             case "ToNick":
                 print(ToNick);
+                break;
+            case "fiona":
+                print(fiona);
+                break;
+            case "dyxxzgc":
+                print(dyxxzgc);
+                break;
+            case "hhyx":
+                print(hhyx);
+                break;
+            case "囧帝":
+                print(囧帝);
+                break;
+            case "kklkit":
+                print(kklkit);
+                break;
+            case "一周茶饭":
+                print(一周茶饭);
+                break;
+            case "一周一个":
+                print(一周一个);
+                break;
+            case "qaz17899":
+                print(qaz17899);
+                break;
+            case "唯恋丶枫叶乀":
+                print(唯恋丶枫叶乀);
                 break;
             default:
                 System.out.println("未找到用户，请检查拼写后重试！");
                 System.out.println("要结束进程吗？(y/n)：");
                 Main.exit = PrintUser.sc.nextLine();
+
         }
 
     }
--- src/io/github/squid233/mcbbsuserinfo/user/Users.java
+++ src/io/github/squid233/mcbbsuserinfo/user/Users.java
@@ -3,6 +3,31 @@ package io.github.squid233.mcbbsuserinfo.user;
 /**
  * 此类用于储存用户。
  */
+@SuppressWarnings("NonAsciiCharacters")
 public final class Users {
-    public static final User ToNick = new User(1, "ToNick", null, "Lv.8 考古家", 3803, 508, 1649, 17, 1, 0, 3, 0, 704);
+
+    public static final User ToNick;
+    public static final User fiona;
+    public static final User dyxxzgc;
+    public static final User hhyx;
+    public static final User 囧帝;
+    public static final User kklkit;
+    public static final User 一周茶饭;
+    public static final User 一周一个;
+    public static final User qaz17899;
+    public static final User 唯恋丶枫叶乀;
+
+    static {
+        ToNick = new User(1, "ToNick", null, "Lv.8 考古家", 3803, 508, 1649, 17, 1, 0, 3, 0, 704);
+        fiona = new User(2, "fiona", null, "Lv.5 农夫", 464, 51, 1517, 10, 0, 0, 0, 0, 124);
+        dyxxzgc = new User(3, "dyxxzgc", null, "Lv-? 禁止发言", 1062, 175, -289, 11, 0, 0, -1, 0, 143);
+        hhyx = new User(4, "hhyx", null, "Lv.8 考古家", 3405, 342, 1048, 38, 3, 0, 3, 0, 853);
+        囧帝 = new User(5, "囧帝", null, "Lv.7 猎手", 1332, 147, 1302, 10, 0, 0, 0, 0, 232);
+        kklkit = new User(6, "kklkit", null, "Lv-? 禁止发言", 259, 55, 1373, 10, 0, 0, 0, 0, 34);
+        一周茶饭 = new User(7, "一周茶饭", null, "Lv.7 猎手", 1765, 180, 115, 40, 3, 0, 0, 0, 399);
+        一周一个 = new User(8, "一周一个", null, "Lv.7 猎手", 1588, 173, 1566, 10, 1, 1, 0, 0, 326);
+        qaz17899 = new User(9, "qaz17899", null,"Lv-? 禁止发言", 290, 36, 1354, 10, 0, 0, 0, 0, 74);
+        唯恋丶枫叶乀 = new User(10, "唯恋丶枫叶乀", null, "Lv.5 农夫", 473, 69, 1387, 13, 0, 0, 0, 0, 100);
+
+    }
 }
